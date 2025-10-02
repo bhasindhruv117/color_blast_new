bd4390afc34b7bae6a1db4fbdcf4b626ec9b4ec3
Dhruv Bhasin <bhasindhruv117@github.com>
2025-10-03 00:49:05 +0530

    Stopped logs

--- Assets/Project Files/Game/Scripts/GameManager.cs
+++ Assets/Project Files/Game/Scripts/GameManager.cs
@@ -11,6 +11,10 @@ public class GameManager : MonoBehaviour
 
     private void Initialize()
     {
+        Debug.unityLogger.logEnabled = false;
+#if DEVELOPMENT_BUILD
+        Debug.unityLogger.logEnabled = true;
+#endif
         Util.SetInstallTime();
         var localNotifications = LocalNotifications.Instance;
     }
